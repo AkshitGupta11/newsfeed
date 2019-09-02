66339b32ab03764e0a43b07291387bafa95c02f9
unknown <akshitgupta881@gmail.com>
2019-09-02 21:15:36 +0530

    init commit

---
+++ src/component/Header.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import './Header.css'
+
+class Header extends Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            title:'React App',
+            keyword:'User text Here'
+        }
+    }
+
+    inputdetect(event){
+        //console.log(event.target.value)
+        this.setState({keyword:event.target.value?event.target.value:'User Text Here'})
+        this.props.newsSearch(event.target.value)
+    }
+    render(){
+        return(
+            <header>
+                <div className="logo"
+                onClick={() => {console.log('logo clicked')}}>
+                {this.state.title}
+                </div>
+                <center>
+                    <input onChange={this.inputdetect.bind(this)}/>
+                    <p>{this.state.keyword}</p>
+                </center>
+                <hr/>
+            </header>
+        )
+    }
+}
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,56 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Header from './component/Header';
+import JSON  from './db.json';
+import NewsList from './component/NewsList'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends Component {
+    constructor(props){
+        super(props)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+        this.state={
+            news:JSON,
+            filtered: JSON
+        }
+    }
+
+    filterNews(keyword){
+        console.log("inside index", keyword)
+        let output=  this.state.news.filter((item) => {
+            return (item.title.toLowerCase().indexOf(keyword.toLowerCase()) > -1 ||
+            item.feed.toLowerCase().indexOf(keyword.toLowerCase()) > -1);
+        })
+        this.setState({filtered: output})
+    }
+
+    /*
+    ages.filter((age) => { return age >= 18; });
+    */
+
+    render(){
+        console.log(this.state.news)
+        return(
+            <div>
+                <Header newsSearch={(userSearch)=>this.filterNews(userSearch)}/>
+                <hr/>
+                <NewsList mynews={this.state.filtered}/>
+            </div>
+        )
+    }
+
+}
+
+ReactDOM.render(<App/>,document.getElementById('root'))
+
+
+
+/*
+const App = () => {
+    return <div>
+                <Header/>
+                <h1>Hello to react</h1>
+                <h2>This is about frist app</h2>
+                <Home/>
+            </div>
+}
+*/
